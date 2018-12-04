bad0da9f4a7e75764d5a5fab01be2bed5caa3373
ChrisBusacker-NDSU <busackerc@hotmail.com>
2018-12-03 23:30:08 -0600

    Artillery update

--- ourProject/tankType/artillery.php
+++ ourProject/tankType/artillery.php
@@ -6,6 +6,7 @@
   <body>
     <h1>|  </h1>
 
+         <h2></h2>
     <div class="navbar">
     <a href="http://students.cs.ndsu.nodak.edu/~ralean/ourProject/home.php">Home</a>
     <br>
@@ -13,7 +14,26 @@
 
     <h1>Artillery!</h1>
 
+	<?php
+	// Remember to replace 'username' and 'password'!
+	$conn = oci_connect('busacker', 'Sep771994', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
 
+	//put your query here
+	$query = "SELECT Tankname, Country.CountryName, Tanktier FROM Tank_List INNER JOIN Country ON Tank_List.CountryId=Country.CountryId WHERE Tanktype = 'SPG' ORDER BY Tanktier";
+	$stid = oci_parse($conn,$query);
+	oci_execute($stid,OCI_DEFAULT);
 
+	//iterate through each row
+	while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
+	{
+    	//iterate through each item in the row and echo it  
+    	foreach ($row as $item)    
+    	{
+        	echo $item.' ';
+    	}   
+	echo '<br/>';}
+	oci_free_statement($stid);
+	oci_close($conn);
+	?>
   </body>
 </html>
