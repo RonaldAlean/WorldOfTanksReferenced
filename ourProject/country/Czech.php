3a61ff6850e6899b521556d621937e56dfee08b8
ChrisBusacker-NDSU <busackerc@hotmail.com>
2018-12-03 23:45:57 -0600

    new country Czech

---
+++ ourProject/country/Czech.php
@@ -0,0 +1,43 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>|  </h1>
+
+    <h2></h2>
+    <div class="navbar">
+    <a href="http://students.cs.ndsu.nodak.edu/~busacker/Final%20Data%20Base/home.php">Home</a>
+    <br>
+    </div>
+
+    
+    <h1>Czech!</h1>
+	
+	<?php
+	// Remember to replace 'username' and 'password'!
+	$conn = oci_connect('busacker', 'Sep771994', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+
+	//put your query here
+	$query = "SELECT Tankname, Tanktype, Tanktier FROM Tank_List WHERE CountryId = 9 ORDER BY Tanktier";
+	$stid = oci_parse($conn,$query);
+	oci_execute($stid,OCI_DEFAULT);
+
+	//iterate through each row
+	while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
+	{
+    	//iterate through each item in the row and echo it  
+    	foreach ($row as $item)    
+    	{
+        	echo $item.' ';
+    	}   
+	echo '<br/>';}
+	oci_free_statement($stid);
+	oci_close($conn);
+	?>
+
+
+
+  </body>
+</html>
