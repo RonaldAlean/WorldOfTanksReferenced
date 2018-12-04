9248dd6ce85b204c17c6bb2b6e209fdc7bdf4439
ChrisBusacker-NDSU <busackerc@hotmail.com>
2018-12-03 23:37:01 -0600

    Germany update

--- ourProject/country/gremany.php
+++ ourProject/country/gremany.php
@@ -5,13 +5,37 @@
   </head>
   <body>
     <h1>|  </h1>
-
+    
+      <h2></h2>
     <div class="navbar">
     <a href="http://students.cs.ndsu.nodak.edu/~ralean/ourProject/home.php">Home</a>
     <br>
     </div>
 
     <h1>Germany!</h1>
+
+	<?php
+	// Remember to replace 'username' and 'password'!
+	$conn = oci_connect('busacker', 'Sep771994', '(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=db2.ndsu.edu)(Port=1521)))(CONNECT_DATA=(SID=cs)))');
+
+	//put your query here
+	$query = "SELECT Tankname, Tanktype, Tanktier FROM Tank_List  WHERE CountryId = 3 ORDER BY Tanktier";
+	$stid = oci_parse($conn,$query);
+	oci_execute($stid,OCI_DEFAULT);
+
+	//iterate through each row
+	while ($row = oci_fetch_array($stid,OCI_ASSOC)) 
+	{
+    	//iterate through each item in the row and echo it  
+    	foreach ($row as $item)    
+    	{
+        	echo $item.' ';
+    	}   
+	echo '<br/>';}
+	oci_free_statement($stid);
+	oci_close($conn);
+	?>
+
 
 
 
